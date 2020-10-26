dde9dd1c36f255796109651800c14d4baeeea0aa
13gi <66826832+13gi@users.noreply.github.com>
2020-10-26 23:56:32 +0300

    Task: 3.2 .. 3.2.3.6

--- u5/U5-1.sql
+++ u5/U5-1.sql
@@ -1,6 +1,6 @@
 
 /* ---------------------------------------
-	Задание 4.2.0
+	Задание 3.2.0
 ------------------------------------------ */
 
 
@@ -15,7 +15,7 @@ from
 
 
 /* ---------------------------------------
-	Задание 4.2.1
+	Задание 3.2.1
 ------------------------------------------ */
 
 select 
@@ -46,7 +46,7 @@ WHERE population > (select avg(population) from shipping.city LIMIT 1)
 
 
 /* ---------------------------------------
-	Задание 4.2.2
+	Задание 3.2.2
 ------------------------------------------ */
 SELECT * FROM shipping.city c 
 
@@ -65,7 +65,7 @@ WHERE area > (select avg(area) from shipping.city LIMIT 1)
 
 
 /* ---------------------------------------
-	Задание 4.3
+	Задание 3.3
 ------------------------------------------ */
 select 	*
 	from
@@ -75,7 +75,7 @@ select 	*
 
 
 /* ---------------------------------------
-	Задание 4.3.1
+	Задание 3.3.1
 ------------------------------------------ */
 
 SELECT *
@@ -86,7 +86,7 @@ JOIN shipping.city c ON s.city_id = c.city_id
 
 
 /* ---------------------------------------
-	Задание 4.3
+	Задание 3.3
 ------------------------------------------ */
 
 select 	
@@ -98,22 +98,22 @@ from
 
 
 /* ---------------------------------------
-	Задание 4.3.2
-ERR
+	Задание 3.3.2
 ------------------------------------------ */
 
 SELECT 
 	--*
-	DISTINCT (s.city_id, c.city_id)
+	DISTINCT s.city_id, c.city_id
 	--s.city_id, c.city_id
 FROM shipping.shipment s 
-LEFT JOIN shipping.customer c ON s.cust_id = c.cust_id 
+JOIN shipping.customer c 
+	ON s.cust_id = c.cust_id 
 
 
 
 
 /* ---------------------------------------
-	Задание 4.3.3
+	Задание 3.3.3
 ------------------------------------------ */
 select 	
 	d.last_name,
@@ -130,7 +130,7 @@ offset 1
 
 
 /* ---------------------------------------
-	Задание 4.3.3
+	Задание 3.3.3
 ------------------------------------------ */
 SELECT 
 	c.city_name
@@ -144,7 +144,7 @@ OFFSET 1 LIMIT 1
 
 
 /* ---------------------------------------
-	Задание 4.3
+	Задание 3.3
 ------------------------------------------ */
 SELECT * FROM shipping.truck 
 
@@ -165,29 +165,95 @@ where t.truck_id = 8
 
 
 /* ---------------------------------------
-	Задание 4.3.4
+	Задание 3.2.3.4
 ------------------------------------------ */
 --	Выведите среднее население, среднюю площадь и плотность населения городов, чья площадь больше города Paramount. 
 --	Плотность населения в этом задании рассчитывается как сумма населения, деленная на сумму площади.
+SELECT * FROM shipping.city
+WHERE city_name = 'Paramount'
 
 
 
-
+SELECT 
+	  avg(c2.population) AS avg_population
+	, avg(c2.area) AS avg_area
+	, sum(c2.population)/sum(c2.area) AS avg_density
+	--	c2.*
+FROM shipping.city c1 
+JOIN shipping.city c2
+	ON c1.area < c2.area AND c1.city_id != c2.city_id 
+WHERE c1.city_id = 109
 
 
 /* ---------------------------------------
-	Задание 4.
+	Задание 3.2.3
 ------------------------------------------ */
+
+select 
+	s.ship_date,
+	t.model_year,
+	c.city_name,
+	count(*) shipments
+from 
+	shipping.shipment s
+		join shipping.truck t on s.truck_id = t.truck_id
+		join shipping.city c on s.city_id = c.city_id
+group by 	
+	s.ship_date,
+	t.model_year,
+	c.city_name
 
 
 /* ---------------------------------------
-	Задание 4.
+	Задание 3.2.3.5
 ------------------------------------------ */
+
+select 
+	  d.first_name
+	, d.last_name
+	, c.city_name 
+	, max(s.ship_date) AS max_ship_date
+from 
+	shipping.driver d
+		join shipping.city c on c.city_id = d.city_id
+		JOIN shipping.shipment s ON d.driver_id = s.driver_id 
+group by 	
+	  d.first_name
+	, d.last_name
+	, c.city_name 
+ORDER BY d.last_name 
+	
 
 
 /* ---------------------------------------
-	Задание 4.
+	Задание 3.2.3.6
+------------------------------------------
+Посчитайте количество доставок и среднюю массу груза в разрезе клиентов и города доставки. 
+Выведите таблицу со следующими полями: 
+	имя клиента, 
+	город доставки, 
+	количество доставок и 
+	средняя масса груза. 
+
+Отсортируйте по имени клиента в алфавитном порядке, затем по массе среднего груза по убыванию.
+
+Подсказка: 
+количество доставок и средняя масса груза содержатся в таблице shipment, 
+имя клиента содержится в таблице customer, а название городов — в таблице city.
+
 ------------------------------------------ */
+
+SELECT 
+	  cu.cust_name 
+	, ci.city_name 
+	, count(s.ship_id) AS count_shipping
+	, avg(s.weight) AS avg_weight
+FROM shipping.shipment as s 
+	JOIN shipping.customer as cu ON cu.cust_id = s.cust_id 
+	JOIN shipping.city AS ci ON ci.city_id = s.city_id 
+GROUP BY cu.cust_name
+	, ci.city_name 
+ORDER BY cu.cust_name, avg_weight DESC 
 
 
 /* ---------------------------------------
