d824507f2b47117c20f85f025a952950f30501bb
13gi <66826832+13gi@users.noreply.github.com>
2020-11-06 18:57:15 +0300

    export ready

--- module_4/u5-dst-project.sql
+++ module_4/u5-dst-project.sql
@@ -412,8 +412,8 @@ from flights f
 where 
 	f.departure_airport = 'AAQ'
 	and f.status != 'Cancelled'
-	and date_part('year', f.actual_arrival) = 2017
-	and date_part('month', f.actual_arrival) in (01, 02)
+	and (f.actual_arrival between '2017-01-01 00:00:00' and '2017-02-28 23:59:59'
+	or f.actual_arrival between '2017-12-01 00:00:00' and '2017-12-31 23:59:59')
 
 
 -- | второе решение
@@ -423,9 +423,16 @@ from flights f
 where 
 	f.departure_airport = 'AAQ'
 	and f.status != 'Cancelled'
-	and f.actual_arrival between '2017-01-01 00:00:00' and '2017-02-28 23:59:59'
-	
-	
+	and date_part('year', f.actual_arrival) = 2017
+	and date_part('month', f.actual_arrival) in (01, 02, 12)	
+
+
+
+
+
+
+
+
 
 -- |----------------------------------------
 -- | Вопрос 3. Посчитайте количество отмененных рейсов из Анапы за все время
@@ -479,9 +486,86 @@ order by 2 desc
 
 
 
+/* -----------------------------------------------------------------------------------
+	6. Переходим к реальной аналитике 
+--------------------------------------------------------------------------------------
+	
+	 Выгрузка данных для анализа
+-------------------------------------------------------------------------------------- */
 
 
-
+with ticket_sold (flight_id, fare_conditions, ticket_count, amount_sold) as (
+	select
+		tf.flight_id,
+		tf.fare_conditions ,
+		count(tf.ticket_no) as ticket_count,
+		sum(tf.amount) as amount_sold
+	from ticket_flights tf 
+	join flights fl on tf.flight_id = fl.flight_id
+	where 
+		fl.departure_airport = 'AAQ'
+		and (date_trunc('month', fl.scheduled_departure) in ('2017-01-01', '2017-02-01', '2017-12-01'))
+		and fl.status not in ('Cancelled')
+	group by tf.flight_id, tf.fare_conditions 
+), 
+flight_ticket_sold (flight_id, class2, ticket_count2, amount_sold2, avg_price_class2, class1, ticket_count1, amount_sold1, avg_price_class1) as (
+	select 
+		t1.flight_id
+		, t1.fare_conditions as class2
+		, t1.ticket_count
+		, t1.amount_sold::int
+		, (t1.amount_sold / t1.ticket_count)::int as avg_price_class2
+		, t2.fare_conditions as class1
+		, t2.ticket_count
+		, t2.amount_sold::int
+		,  (t2.amount_sold / t2.ticket_count)::int as avg_price_class1
+	from ticket_sold t1
+	join ticket_sold t2 on t2.flight_id = t1.flight_id 
+		and t1.fare_conditions != t2.fare_conditions
+		and t1.fare_conditions = 'Economy'
+) --select * from flight_ticket_sold
+select
+	f.flight_id, 	
+	date_part('year', f.scheduled_departure)::int as sched_year,
+	date_part('month', f.scheduled_departure)::int as sched_month,
+	date_part('day', f.scheduled_departure)::int as sched_day,
+	f.departure_airport,
+	f.flight_no,
+	f.arrival_airport,
+	f.scheduled_departure,
+	f.scheduled_arrival,
+	f.actual_departure - f.scheduled_departure as delay_departure,
+	f.status,
+	f.actual_arrival - f.actual_departure as flight_duration,
+	a2.city,
+	a2.timezone,
+	f.actual_departure,
+	f.actual_arrival,
+	f.actual_arrival - f.scheduled_arrival as delay_arrival,
+	f.aircraft_code,
+	split_part(a.model, ' ', 1) as manufacturer,
+	split_part(a.model, ' ', 2) as board_model, 
+	a."range" as flights_range_km,
+	--a.model as full_model,
+	fts.class2, 
+	fts.ticket_count2, 
+	fts.amount_sold2, 
+	fts.avg_price_class2, 
+	fts.class1, 
+	fts.ticket_count1, 
+	fts.amount_sold1, 
+	fts.avg_price_class1,
+	(fts.amount_sold2 + fts.amount_sold1)::int as total_amount
+from
+	flights as f
+join aircrafts a on a.aircraft_code = f.aircraft_code
+join airports a2 on a2.airport_code = f.arrival_airport 
+left join flight_ticket_sold fts on f.flight_id = fts.flight_id
+where
+	f.departure_airport = 'AAQ'
+	and (date_trunc('month', f.scheduled_departure) in ('2017-01-01', '2017-02-01', '2017-12-01'))
+	and f.status not in ('Cancelled')
+order by f.scheduled_departure 
 
 
 
